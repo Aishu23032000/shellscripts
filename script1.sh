2b15a991175bb449142cf7c0bb51934326ffb30b
Aishu23032000 <aishwaryadeshpande2303@gmail.com>
2025-08-12 13:41:02 +0530

    committed

--- script1.sh
+++ script1.sh
@@ -1,7 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
-echo "I am learning branching today "
-echo "hiiiii"
-echo " hello"
+echo "I am learning it agin"
 
 
